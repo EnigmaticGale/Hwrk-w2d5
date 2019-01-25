dce39c8c27d9f116f69fa08c68e8de30b3aca139
EnigmaticGale <hamishwhytehw@gmail.com>
2019-01-25 14:24:14 +0000

    song read test working

--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -8,4 +8,10 @@ class TestSong < MiniTest::Test
     @song_1 = Song.new("Song Title" ,"Artist" ,180)
   end
 
+  def test_read_song_data
+    assert_equal("Song Title", @song_1.title)
+    assert_equal("Artist", @song_1.artist)
+    assert_equal(180, @song_1.duration)
+  end
+
 end
